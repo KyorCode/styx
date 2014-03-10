c1cf5d8d07b6ba93ff59a1c779a51291c1ee7222
KyorCode <t-c@live.be>
2014-03-10 08:51:15 +0100

    isDefault override added

--- README.md
+++ README.md
@@ -6,14 +6,15 @@ Styx is a very simple IoC ( Inversion of Control ) container for Node. Based on 
 
 *need to order these*
 
-0. Installers
-1. Interceptor
-2. register directory
-3. register require call to a module
-4. just in time resolve
-5. lifeStyle management
-6. Circular dependency detection
-7. Resolve submodules
-8. Factories
-9. Multiple resolvers, array injections
+0.  Installers
+1.  Interceptor
+2.  register directory
+3.  register require call to a module
+4.  just in time resolve
+5.  lifeStyle management
+6.  Circular dependency detection
+7.  Resolve submodules
+8.  Factories
+9.  Multiple resolvers, array injections
+10. eventemitter container
 
--- test/styx.test.js
+++ test/styx.test.js
@@ -3,16 +3,27 @@ var styx = require('./../lib/styx')
 
 describe("Styx", function () {
 
-    beforeEach(function(){
-       debugger;
-       this.container = styx.create();
+    beforeEach(function () {
+        debugger;
+        this.container = styx.create();
     });
 
     it('can be constructed', function () {
         expect(this.container).to.exist;
     });
 
-    it('has only the container registered',function(){
-       expect(this.container.get('container')).to.equal(this.container);
+    it('has only the container registered', function () {
+        expect(this.container.get('container')).to.equal(this.container);
+    });
+
+    it('can not register the same name twice', function () {
+        this.container.register('container', {});
+        expect(this.container.get('container')).to.equal(this.container);
+    });
+
+    it('can register the same name twice if isDefault is provided', function () {
+        var obj = {};
+        this.container.register('container', obj, { isDefault: true});
+        expect(this.container.get('container')).to.equal(obj);
     });
 });
